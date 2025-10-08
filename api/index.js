72f5c17a12686b6e94b357f9aa23b89a7b16c4ca
Bella Wei <weiyue1996@gmail.com>
2025-10-07 19:22:21 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connect to MongoDB!');}).catch((err)=>{
+    console.log(err);
+});
 
 const app = express()
 app.listen(4000, ()=> {
-    console.log('Server is running on port 4000.');
+    console.log('Server is running on port 4000!');
 })
