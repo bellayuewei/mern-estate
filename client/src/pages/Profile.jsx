418f7db2d793cffc747042cbb1487a4e9c482137
Bella Wei <weiyue1996@gmail.com>
2025-10-28 14:20:18 -0700

    add delete user functionally

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { useRef} from 'react'
 import { useState,useEffect } from 'react'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { app } from '../firebase'
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js'
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js'
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -83,6 +83,22 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
+  const handleDeleteUser = async()=>{
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if (data.success=== false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch(error){
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (<div className='p-3 max-w-lg mx-auto'>
   <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
   <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
@@ -104,7 +120,7 @@ export default function Profile() {
     <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading? 'Loading...':'Update'}</button>
   </form>
   <div className="flex justify-between mt-5">
-    <span className='text-red-700 cursor-pointer'>Delete account</span>
+    <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
     <span className='text-red-700 cursor-pointer'>Sign Out</span>
   </div>
 
